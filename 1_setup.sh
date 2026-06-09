3fac3b1852a227092b2593abd87974ec73cc10ed
Edwin Campos <ecamposc97@gmail.com>
2026-06-09 15:43:35 -0500

    .

--- 1_setup.sh
+++ 1_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp .env /usr/local/etc/.conteo.env
-cp "conteo-personas/conteo-personas.sh" /usr/local/bin/
-cp "ui/ui-totem.sh" /usr/local/bin/
+sudo cp .env /usr/local/etc/.conteo.env
+sudo cp "conteo-personas/conteo-personas.sh" /usr/local/bin/
+sudo cp "ui/ui-totem.sh" /usr/local/bin/
 cp ui/ui-totem.desktop "$HOME/.config/autostart/"
 sudo chmod +x "$HOME/.config/autostart/*.desktop"
-chmod +x /usr/local/bin/*sh
+sudo chmod +x /usr/local/bin/*sh
